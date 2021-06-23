d9320f704b022684811bd1e02c0b43cc66f4af31
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-06-23 09:59:43 +0300

    editing code

--- structuer/app.js
+++ structuer/app.js
@@ -131,8 +131,8 @@ for (i = 1; i <= 4; i++) {
 if (Q6 <= 5 || Q6 >= 50) {
     alert("the correct  Considered numbers to answer is  " + Arr1);
 }
-else if (Q6 >= 5 || Q6 <= 50)
-{
+else if (Q6 >= 5 || Q6 <= 50) {
+    alert(" correct (^_^) ");
     add();
 }
 
